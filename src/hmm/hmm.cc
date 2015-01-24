16ab9d16b58d754f4751d264c5c27265bcf4d60d
Xiang Gong <xgong@ece.neu.edu>
2015-01-24 18:18:13 -0500

    Update

--- src/hmm/hmm.cc
+++ src/hmm/hmm.cc
@@ -7,19 +7,48 @@
 HMM::HMM(int N)
 {
 	this->N = N;
-
-	// Init OCL context
-	runtime = clRuntime::getInstance();
-	runtime->displayAllInfo();
-
-	file = clFile::getInstance();
-
 }
 
 HMM::~HMM()
 {
 	// Some cleanup
 	Release();
+}
+
+void HMM::SetupCL()
+{
+	// Init OCL context
+	runtime = clRuntime::getInstance();
+	device = runtime->getDevice();
+	context = runtime->getContext();
+
+	// Helper to read kernel file
+	file = clFile::getInstance();
+	file->open("hmm_Kernels.cl");
+
+
+	cl_int err;
+	// Create program
+	const char *source = file->getSourceChar();
+
+	printf("%s\n", source);
+	program = clCreateProgramWithSource(context, 1, 
+		(const char**)&source, NULL, &err);
+	checkOpenCLErrors(err, "Failed to create Program with source...\n");
+
+	// Create kernel with OpenCL 2.0 support
+	err = clBuildProgram(program, 0, NULL, "-cl-std=CL2.0", NULL, NULL);
+	checkOpenCLErrors(err, "Failed to build program...\n");
+
+	char buf[0x10000];
+	clGetProgramBuildInfo( program,
+				device,
+				CL_PROGRAM_BUILD_LOG,
+				0x10000,
+				buf,
+				NULL);
+	printf("\n%s\n", buf);
+
 }
 
 void HMM::Param()
@@ -47,6 +76,46 @@ void HMM::Release()
 
 }
 
+void HMM::Run()
+{
+
+	SetupCL();
+
+	//-------------------------------------------------------------------------------------------//
+	// HMM Parameters
+	//	a,b,pi,alpha
+	//-------------------------------------------------------------------------------------------//
+	printf("=>Initialize parameters.\n");
+	Param();
+
+	//-------------------------------------------------------------------------------------------//
+	// Forward Algorithm on GPU 
+	//-------------------------------------------------------------------------------------------//
+	printf("\n");
+	printf("      >> Start  Forward Algorithm on GPU.\n");
+	Forward();
+	printf("      >> Finish Forward Algorithm on GPU.\n");
+
+	//-------------------------------------------------------------------------------------------//
+	// Forward Algorithm on GPU 
+	//-------------------------------------------------------------------------------------------//
+	printf("\n");
+	printf("      >> Start  Backward Algorithm on GPU.\n");
+	Backward();
+	printf("      >> Finish Backward Algorithm on GPU.\n");
+
+	//-------------------------------------------------------------------------------------------//
+	// Baum-Welch Algorithm on GPU 
+	//-------------------------------------------------------------------------------------------//
+	printf("\n");
+	printf("      >> Start  Baum-Welch Algorithm on GPU.\n");
+	BaumWelch();
+	printf("      >> Finish Baum-Welch Algorithm on GPU.\n");
+
+	printf("<=End program.\n");
+
+}
+
 int main(int argc, char const *argv[])
 {
 	if(argc != 2){
@@ -58,41 +127,10 @@ int main(int argc, char const *argv[])
 
 	int N = atoi(argv[1]);
 
-	// Smart pointer, auto cleanup
+	// Smart pointer, auto cleanup in destructor
 	std::unique_ptr<HMM> hmm(new HMM(N));
 
-	//-------------------------------------------------------------------------------------------//
-	// HMM Parameters
-	//	a,b,pi,alpha
-	//-------------------------------------------------------------------------------------------//
-	printf("=>Initialize parameters.\n");
-	hmm->Param();
-
-	//-------------------------------------------------------------------------------------------//
-	// Forward Algorithm on GPU 
-	//-------------------------------------------------------------------------------------------//
-	printf("\n");
-	printf("      >> Start  Forward Algorithm on GPU.\n");
-	hmm->Forward();
-	printf("      >> Finish Forward Algorithm on GPU.\n");
-
-	//-------------------------------------------------------------------------------------------//
-	// Forward Algorithm on GPU 
-	//-------------------------------------------------------------------------------------------//
-	printf("\n");
-	printf("      >> Start  Backward Algorithm on GPU.\n");
-	hmm->Backward();
-	printf("      >> Finish Backward Algorithm on GPU.\n");
-
-	//-------------------------------------------------------------------------------------------//
-	// Baum-Welch Algorithm on GPU 
-	//-------------------------------------------------------------------------------------------//
-	printf("\n");
-	printf("      >> Start  Baum-Welch Algorithm on GPU.\n");
-	hmm->BaumWelch();
-	printf("      >> Finish Baum-Welch Algorithm on GPU.\n");
-
-	printf("<=End program.\n");
+	hmm->Run();
 
 	return 0;
 }
--- src/hmm/hmm.h
+++ src/hmm/hmm.h
@@ -10,17 +10,44 @@ class HMM
 	clRuntime *runtime;
 	clFile *file;
 
+	cl_device_id device;
+	cl_context context;
+	cl_program program;
+
+	cl_kernel kernel_FWD_init_alpha;
+	cl_kernel kernel_FWD_scaling;
+	cl_kernel kernel_FWD_calc_alpha;
+	cl_kernel kernel_FWD_sum_ll;
+	cl_kernel kernel_BK_update_beta;
+	cl_kernel kernel_BK_scaling;
+	cl_kernel kernel_EM_betaB_alphabeta;
+	cl_kernel kernel_EM_alphabeta_update_gamma;
+	cl_kernel kernel_EM_A_mul_alphabetaB;
+	cl_kernel kernel_EM_update_xisum;
+	cl_kernel kernel_EM_alphabeta;
+	cl_kernel kernel_EM_expect_A;
+	cl_kernel kernel_EM_transpose;
+	cl_kernel kernel_EM_gammastatesum;
+	cl_kernel kernel_EM_gammaobs;
+	cl_kernel kernel_EM_expectmu;
+	cl_kernel kernel_EM_expectsigma_dev;
+	cl_kernel kernel_EM_update_expectsigma;	
+
 	int N;
+
+	void SetupCL();
+	void Param();
+	void Forward();
+	void Backward();
+	void BaumWelch();
 
 	void Release();
 public:
 	HMM(int N);
 	~HMM();
 
-	void Param();
-	void Forward();
-	void Backward();
-	void BaumWelch();
+	void Run();
+
 };
 
 #endif
