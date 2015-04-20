bf725ce7924ccff49b63aff0d45d513a540cb7b5
Yifan Sun <sunyifan112358@gmail.com>
2015-04-20 13:19:19 -0400

    A command line option parser

--- hsa/iir_cl20/main.cc
+++ hsa/iir_cl20/main.cc
@@ -1,77 +1,69 @@
 #include <cstring>
 
+#include "../common/OptionParser.h"
+
 #include "IirFilter.h"
 
-void DumpHelpInfo()
+uint32_t len = 1024;
+bool is_verification_mode = false;
+
+void ParseArgument(int argc, char **argv)
 {
-	static const char help_info[] = 
-		"IIR Filter benchmark\n\n" 
-		"Syntax: IIR [options]\n\n" 
-		"Options: \n" 
-		"-h --help: Show this page.\n" 
-		"-l --length: Data length\n";
-	printf("%s", help_info);
+	OptionParser parser = OptionParser(argv[0], 
+			"The IIR filter benchmark");
+
+	// Set length argument
+	parser.AddArgument(
+			"Data Length", 
+			"-l", "--length",
+			"The length of the data to be processed", 
+			"1024",
+			typeid(unsigned int)
+			);
+
+	// Set verification mode argument
+	parser.AddArgument(
+			"Verification Mode", 
+			"-v", "--verify",
+			"Enter verification mode", 
+			"false",
+			typeid(bool)
+			);	
+
+	// Parse argument
+	parser.Parse(argc, argv);
+
+	// Get length
+	len = parser.getValue<uint32_t>("Data Length");
+	printf("Len: %d\n", len);
+
+	// Get verification mode
+	is_verification_mode = parser.getValue<bool>("Verification Mode");
+	printf("Verification mode: %s\n", is_verification_mode?"true":"false");
 }
+
 
 int main(int argc, char **argv) 
 {
-	uint32_t len = 1024;
-	bool is_verification_mode = false;
-
 	// Parse argument
-	for (int i = 1; i < argc; i++)
-	{
-		if (strcmp("-h", argv[i]) == 0 ||
-			strcmp("--help", argv[i]) == 0)
-		{
-			DumpHelpInfo();
-			exit(0);
-		}
-		else if (strcmp("-l", argv[i]) == 0 || 
-			strcmp("--length", argv[i]) == 0)
-		{
-			if (i == argc - 1) 
-			{
-				DumpHelpInfo();
-				exit(0);
-			}
-			else 
-			{
-				try 
-				{
-					len = stoi(std::string(argv[i+1]));
-				}
-				catch (std::exception) 
-				{
-					printf("Invalid number for argument "
-							"len\n");
-					DumpHelpInfo();
-					exit(0);
-				}
-				i++;
-			}
-		}
-		else if (strcmp("-v", argv[i]) == 0 ||
-			strcmp("--verify", argv[i]) == 0)
-		{
-			is_verification_mode = true;
-		}
-		else
-		{
-			printf("Unknown argument\n");
-			DumpHelpInfo();
-			exit(1);
-		}
-	}
-		
+	ParseArgument(argc, argv);
+
+	// Setup runtime helper
 	HsaHelper helper = HsaHelper();
 	helper.setVerificationMode(is_verification_mode);
 
+	// Init benchmark
 	IirFilter iir = IirFilter();
 	iir.setHelper(&helper);
 	iir.setDataLength(len);
 	iir.Init();
+
+	// Run benchmark
 	iir.Run();
+
+	// Verify execution result
 	if (is_verification_mode)
 		iir.Verify();
+
+	// Summarize
 }
