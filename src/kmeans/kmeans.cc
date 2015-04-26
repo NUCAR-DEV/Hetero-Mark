cde817f30a011ceedbad40b6a46cede699bfd416
Leiming Yu <leimingyu830@gmail.com>
2015-04-26 11:27:41 -0400

    kmeans reformat

--- src/kmeans/kmeans.cc
+++ src/kmeans/kmeans.cc
@@ -251,7 +251,7 @@ void KMEANS::CL_create_kernels()
 	checkOpenCLErrors(err, "Failed to create kernel kmeans_swap");
 }
 
-void KMEANS::CL_create_buffers(int nclusters)
+void KMEANS::Create_mem()
 {
 	cl_int err;
 
@@ -285,6 +285,8 @@ void KMEANS::CL_create_buffers(int nclusters)
 							   NULL, 
 							   &err);
 	checkOpenCLErrors(err, "clCreateBuffer d_cluster failed");
+
+	membership_OCL = (int*) malloc(npoints * sizeof(int));
 }
 
 
@@ -323,7 +325,209 @@ void KMEANS::Swap_features()
 	checkOpenCLErrors(err, "ERROR: clEnqueueNDRangeKernel()");
 }
 	
+
+void KMEANS::Free_mem()
+{
+	clReleaseMemObject(d_feature);
+	clReleaseMemObject(d_feature_swap);
+	clReleaseMemObject(d_cluster);
+	clReleaseMemObject(d_membership);
+
+	free(membership_OCL);
+}
 	
+void KMEANS::Kmeans_ocl()
+{
+	int i, j, k;
+
+	cl_int err;
+
+	// Ke Wang adjustable local group size 2013/08/07 10:37:33
+	// work group size is defined by RD_WG_SIZE_1 or RD_WG_SIZE_1_0 2014/06/10 17:00:41
+	size_t global_work     = (size_t) npoints;
+	size_t local_work_size = BLOCK_SIZE2; 
+
+	if(global_work % local_work_size !=0)
+		global_work = (global_work / local_work_size + 1) * local_work_size;
+
+	// fixme: use svm
+	err = clEnqueueWriteBuffer(cmd_queue, 
+	                           d_cluster, 
+							   1, 
+							   0, 
+							   nclusters * nfeatures * sizeof(float), 
+							   clusters[0], 
+							   0, 0, 0);
+	checkOpenCLErrors(err, "ERROR: clEnqueueWriteBuffer d_cluster");
+
+	int size = 0; int offset = 0;
+
+	clSetKernelArg(kernel_s, 0, sizeof(void *), (void*) &d_feature_swap);
+	clSetKernelArg(kernel_s, 1, sizeof(void *), (void*) &d_cluster);
+	clSetKernelArg(kernel_s, 2, sizeof(void *), (void*) &d_membership);
+	clSetKernelArg(kernel_s, 3, sizeof(cl_int), (void*) &npoints);
+	clSetKernelArg(kernel_s, 4, sizeof(cl_int), (void*) &nclusters);
+	clSetKernelArg(kernel_s, 5, sizeof(cl_int), (void*) &nfeatures);
+	clSetKernelArg(kernel_s, 6, sizeof(cl_int), (void*) &offset);
+	clSetKernelArg(kernel_s, 7, sizeof(cl_int), (void*) &size);
+
+	err = clEnqueueNDRangeKernel(cmd_queue, 
+	                             kernel_s, 
+								 1, 
+								 NULL, 
+								 &global_work, 
+								 &local_work_size, 
+								 0, 0, 0);
+	checkOpenCLErrors(err, "ERROR: clEnqueueNDRangeKernel(kernel_s)");
+
+	clFinish(cmd_queue);
+
+	// fixme : use svm
+	err = clEnqueueReadBuffer(cmd_queue, 
+	                          d_membership, 
+							  1, 
+							  0, 
+							  npoints * sizeof(int), 
+							  membership_OCL, 
+							  0, 0, 0);
+	checkOpenCLErrors(err, "ERROR: Memcopy Out"); 
+
+
+	int delta_tmp = 0;
+	for (i = 0; i < npoints; i++)
+	{
+		int cluster_id = membership_OCL[i];
+		new_centers_len[cluster_id]++;
+		if (membership_OCL[i] != membership[i])
+		{
+			// update membership
+			delta_tmp++;
+			membership[i] = membership_OCL[i];
+		}
+		for (j = 0; j < nfeatures; j++)
+		{
+			new_centers[cluster_id][j] += feature[i][j];
+		}
+	}
+
+	delta = (float) delta_tmp;
+}
+
+
+
+
+
+void KMEANS::Kmeans_clustering()
+{
+	int      i, j, n = 0;		// counters
+	int		 loop=0, temp;
+	//int     *new_centers_len;	// [nclusters]: no. of points in each cluster
+	//float    delta;				// if the point moved
+	//float  **clusters;			// out: [nclusters][nfeatures]
+	//float  **new_centers;		// [nclusters][nfeatures]
+
+	//int     *initial;			// used to hold the index of points not yet selected
+
+	//   prevents the "birthday problem" of dual selection (?)
+	//   considered holding initial cluster indices, but changed due to
+	//   possible, though unlikely, infinite loops
+	int      initial_points = npoints;
+	int		 c = 0;
+
+	// nclusters should never be > npoints
+	// that would guarantee a cluster without points
+	if (nclusters > npoints) 
+	{
+		nclusters = npoints;
+	}
+
+	// fixme : use svm 
+	// allocate space for and initialize returning variable clusters[]
+	clusters    = (float**) malloc(nclusters *             sizeof(float*));
+	clusters[0] = (float*)  malloc(nclusters * nfeatures * sizeof(float));
+	for (i = 1; i < nclusters; i++) 
+	{
+		clusters[i] = clusters[i-1] + nfeatures;
+	}
+
+	// initialize the random clusters
+	initial = (int *) malloc (npoints * sizeof(int));
+	for (i = 0; i < npoints; i++)
+	{
+		initial[i] = i;
+	}
+
+
+	// fixme
+	// randomly pick cluster centers
+	for (i = 0; i < nclusters && initial_points >= 0; i++) 
+	{
+		//n = (int)rand() % initial_points;		
+
+		for (j=0; j<nfeatures; j++)
+			clusters[i][j] = feature[initial[n]][j];	// remapped
+
+		// swap the selected index to the end (not really necessary,
+		// could just move the end up)
+		temp                      = initial[n];
+		initial[n]                = initial[initial_points-1];
+		initial[initial_points-1] = temp;
+		initial_points--;
+		n++;
+	}
+
+	// initialize the membership to -1 for all
+	// fixme: use svm
+	for (i=0; i < npoints; i++) 
+	{
+		membership[i] = -1;
+	}
+
+	// allocate space for and initialize new_centers_len and new_centers
+	new_centers_len = (int*) calloc(nclusters, sizeof(int));
+
+	new_centers    = (float**) malloc(nclusters *            sizeof(float*));
+	new_centers[0] = (float*)  calloc(nclusters * nfeatures, sizeof(float));
+	for (i=1; i<nclusters; i++)
+		new_centers[i] = new_centers[i-1] + nfeatures;
+
+	// iterate until convergence
+	do 
+	{
+		Kmeans_ocl();
+
+		// replace old cluster centers with new_centers
+		// CPU side of reduction
+		for (i=0; i<nclusters; i++) 
+		{
+			for (j=0; j<nfeatures; j++) 
+			{
+				if (new_centers_len[i] > 0)
+				{	// take average i.e. sum/n 
+					clusters[i][j] = new_centers[i][j] / new_centers_len[i];
+				}
+				new_centers[i][j] = 0.0;	// set back to 0
+			}
+			new_centers_len[i] = 0;			// set back to 0 
+		}	 
+
+		c++;
+
+	} while ((delta > threshold) && (loop++ < 500));	// makes sure loop terminates
+	
+
+	printf("iterated %d times\n", c);
+
+	free(new_centers[0]);
+	free(new_centers);
+	free(new_centers_len);
+
+	// clusters is pointed to tmp_cluster_centres 
+	// return clusters;
+	tmp_cluster_centres = clusters;
+}
+
+
 void KMEANS::Clustering()
 {
 	cluster_centres = NULL;
@@ -333,7 +537,6 @@ void KMEANS::Clustering()
 	membership = (int*) malloc(npoints * sizeof(int));
 
 
-
 	CL_initialize();
 	CL_build_program();
 	CL_create_kernels();
@@ -341,7 +544,9 @@ void KMEANS::Clustering()
 
 	min_rmse_ref = FLT_MAX;		
 
-	int	nclusters;			    // number of clusters
+	int i;
+	//int	nclusters;			    // number of clusters
+
 	// sweep k from min to max_nclusters to find the best number of clusters
 	for(nclusters = min_nclusters; nclusters <= max_nclusters; nclusters++)
 	{
@@ -350,54 +555,180 @@ void KMEANS::Clustering()
 			break;	
 
 		// allocate device memory, invert data array
-		CL_create_buffers(nclusters);
-
+		Create_mem();
 		Swap_features();
 
-/*
 		// iterate nloops times for each number of clusters //
 		for(i = 0; i < nloops; i++)
 		{
-			// initialize initial cluster centers, CUDA calls (@ kmeans_cuda.cu) //
-			tmp_cluster_centres = kmeans_clustering(features,
-					nfeatures,
-					npoints,
-					nclusters,
-					threshold,
-					membership);
 
-			if (*cluster_centres) {
-				free((*cluster_centres)[0]);
-				free(*cluster_centres);
+			Kmeans_clustering();
+
+
+			if (cluster_centres) 
+			{
+				free(cluster_centres[0]);
+				free(cluster_centres);
 			}
 
-			*cluster_centres = tmp_cluster_centres;
+			cluster_centres = tmp_cluster_centres;
 
 			// find the number of clusters with the best RMSE //
 			if(isRMSE)
 			{
-				rmse = rms_err(features,
-						nfeatures,
-						npoints,
-						tmp_cluster_centres,
-						nclusters);
+				RMS_err();
 
 				if(rmse < min_rmse_ref){
 					min_rmse_ref = rmse;		//update reference min RMSE
-					*min_rmse = min_rmse_ref;	//update return min RMSE
-					*best_nclusters = nclusters;	//update optimum number of clusters
+					min_rmse = min_rmse_ref;	//update return min RMSE
+					best_nclusters = nclusters;	//update optimum number of clusters
 					index = i;			//update number of iteration to reach best RMSE
 				}
 			}			
 		}
-*/
 
-		//deallocateMemory(); // free device memory
+		Free_mem(); // free device memory
 	}
 
-	// free(membership);
+	free(membership);
+}
+
+// multi-dimensional spatial Euclid distance square
+float KMEANS::euclid_dist_2(float *pt1, 
+                            float *pt2)
+{                                                                               
+	int i;                                                                      
+	float ans=0.0;                                                              
+
+	for (i=0; i<nfeatures; i++)                                                   
+		ans += (pt1[i]-pt2[i]) * (pt1[i]-pt2[i]);                               
+
+	return(ans);                                                                
+}
+
+int KMEANS::find_nearest_point(float  *pt,           // [nfeatures]
+                               float  **pts)         // [npts][nfeatures]
+{                                                                               
+	int index_local, i;                                                               
+	float max_dist = FLT_MAX;                                                     
+
+	// find the cluster center id with min distance to pt
+	for (i=0; i<nclusters; i++) 
+	{                                                    
+		float dist;                                                             
+		dist = euclid_dist_2(pt, pts[i]);  // no need square root
+		if (dist < max_dist) 
+		{                                                  
+			max_dist = dist;                                                    
+			index_local   = i;                                                       
+		}                                                                       
+	}                                                                           
+	return(index_local);                                                              
+}                                                                           
+
+
+void KMEANS::RMS_err()
+{
+	int    i;                                                                   
+	int   nearest_cluster_index;    // cluster center id with min distance to pt
+	float  sum_euclid = 0.0;        // sum of Euclidean distance squares
+	float  ret;                     // return value
+
+	// pass data pointers
+	float **feature_loc, **cluster_centres_loc; 	
+	feature_loc = feature;
+	cluster_centres_loc = tmp_cluster_centres;
+
+	// calculate and sum the sqaure of euclidean distance
+#pragma omp parallel for \
+  shared(feature_loc, cluster_centres_loc) \
+  firstprivate(npoints, nfeatures, nclusters) \
+  private(i, nearest_cluster_index) \
+  schedule (static)  
+
+		for (i=0; i<npoints; i++) 
+		{                                                 
+
+			nearest_cluster_index = find_nearest_point(feature_loc[i],
+					                                   cluster_centres_loc);
+
+			
+			sum_euclid += euclid_dist_2(feature_loc[i],                                 
+					                    cluster_centres_loc[nearest_cluster_index]);
+				
+		}                                                                           
+
+	// divide by n, then take sqrt
+	rmse = sqrt(sum_euclid / npoints);                                           
+}
+
+void KMEANS::Display_results()
+{
+	int i, j;
+
+	// cluster center coordinates : displayed only for when k=1
+	if((min_nclusters == max_nclusters) && (isOutput == 1)) 
+	{                   
+		printf("\n================= Centroid Coordinates =================\n"); 
+		for(i = 0; i < max_nclusters; i++){                                     
+			printf("%d:", i);                                                   
+			for(j = 0; j < nfeatures; j++){                                     
+				printf(" %.2f", cluster_centres[i][j]);                         
+			}                                                                   
+			printf("\n\n");                                                     
+		}                                                                       
+	}                                                                           
+
+	float	len = (float) ((max_nclusters - min_nclusters + 1)*nloops);                 
+
+	printf("Number of Iteration: %d\n", nloops);                                
+	//printf("Time for I/O: %.5fsec\n", io_timing);                             
+	//printf("Time for Entire Clustering: %.5fsec\n", cluster_timing);          
+
+	if(min_nclusters != max_nclusters)
+	{                                         
+		if(nloops != 1)
+		{                                                        
+			//range of k, multiple iteration                                    
+			//printf("Average Clustering Time: %fsec\n",                        
+			//      cluster_timing / len);                                      
+			printf("Best number of clusters is %d\n", best_nclusters);          
+		}                                                                       
+		else
+		{                                                                   
+			//range of k, single iteration                                      
+			//printf("Average Clustering Time: %fsec\n",                        
+			//      cluster_timing / len);                                      
+			printf("Best number of clusters is %d\n", best_nclusters);          
+		}                                                                       
+	}                                                                           
+	else
+	{                                                                       
+		if(nloops != 1)
+		{                                                        
+			// single k, multiple iteration                                     
+			//printf("Average Clustering Time: %.5fsec\n",                      
+			//      cluster_timing / nloops);                                   
+			if(isRMSE) 
+			{// if calculated RMSE                                   
+				printf("Number of trials to approach the best RMSE of %.3f is %d\n",
+						min_rmse, index + 1);
+			}                                                                   
+		}                                                                       
+		else
+		{                                                                   
+			// single k, single iteration                                       
+			if(isRMSE)
+			{                                                         
+				// if calculated RMSE                                           
+				printf("Root Mean Squared Error: %.3f\n", min_rmse);                
+			}                                                                   
+		}                                                                       
+	}                                                                           
 
 }
+
+
 
 void KMEANS::Run(int argc, char **argv)                                         
 {                                                                               
@@ -408,7 +739,9 @@ void KMEANS::Run(int argc, char **argv)
 	//cluster_timing = omp_get_wtime();		// Total clustering time
 	Clustering();
 	//cluster_timing = omp_get_wtime() - cluster_timing;	
-		                                                                                
+
+	//
+	Display_results();	                                                                                
 }    
 
 
@@ -422,7 +755,7 @@ int main( int argc, char** argv)
 		BLOCK_SIZE, BLOCK_SIZE2);
 
 	kmeans->Run(argc, argv);
-	//setup(argc, argv);
+
 	//shutdown();
 
 	return 0;
