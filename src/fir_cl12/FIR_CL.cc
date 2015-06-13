d1d045297336725fd296812f9144c2fde388dbd3
saonim <sayani.mookherjee@gmail.com>
2015-06-13 13:17:07 -0400

    Clock changed in FIR12

--- src/fir_cl12/FIR_CL.cc
+++ src/fir_cl12/FIR_CL.cc
@@ -1,22 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdint.h>/* for uint64 definition */
+#include <stdlib.h>/* for exit() definition */
+#include <time.h>/* for clock_gettime */
 #include <string.h>
 #include "eventlist.h"
 #include <CL/cl.h>
+
 //#ifdef GPUPROF
 //#include "inc/GPUPerfAPI.h"
 //#include <dlfcn.h>
 //#endif
 
+#define BILLION 1000000000L
 
-#define CHECK_STATUS( status, message )   \
-		if(status != CL_SUCCESS) \
-		{ \
-			printf( message); \
-			printf( "\n" ); \
-			return 1; \
-		}
+#define CHECK_STATUS( status, message )		\
+  if(status != CL_SUCCESS)			\
+    {						\
+      printf( message);				\
+      printf( "\n" );				\
+      return 1;					\
+    }
 
 
 
@@ -39,297 +42,303 @@ cl_float* temp_output = NULL;
 
 
 int main(int argc , char** argv) {
-        clock_t c_start, c_stop;
-        c_start = clock();
-	
-	/** Define Custom Variables */
-	int i,count;
-	int local;
+  // clock_t c_start, c_stop;
+  // c_start = clock();
 
-	if (argc < 3)
-	{
-		printf(" Usage : ./auto_exec.sh <numBlocks> <numData>\n");
-		exit(0);
-	}
-	if (argc > 1)
-	{
-		numBlocks = atoi(argv[1]);
-		numData = atoi(argv[2]);
-	}
+  uint64_t diff;
+  struct timespec start, end;
+  clock_gettime(CLOCK_MONOTONIC, &start);/* mark start time */
 
+  /** Define Custom Variables */
+  int i,count;
+  int local;
 
-	/** Declare the Filter Properties */
-	numTap = 1024;
-	numTotalData = numData * numBlocks;
-	local = 64;
-
-	printf("FIR Filter\n Data Samples : %d \n NumBlocks : %d \n Local Workgroups : %d\n", numData,numBlocks,local);
-	//exit(0);
-	/** Define variables here */
-	input = (cl_float *) malloc( numTotalData* sizeof(cl_float) );
-	output = (cl_float *) malloc( numTotalData* sizeof(cl_float) );
-	coeff = (cl_float *) malloc( numTap* sizeof(cl_float) );
-	temp_output = (cl_float *) malloc( (numData+numTap-1) * sizeof(cl_float) );
-
-	/** Initialize the input data */
-	for( i=0;i<numTotalData;i++ )
-	{
-		input[i] = 8;
-		output[i] = 99;
-	}
-
-	for( i=0;i<numTap;i++ )
-		coeff[i] = 1.0/numTap;
-
-	for( i=0; i<(numData+numTap-1); i++ )
-		temp_output[i] = 0.0;
+  if (argc < 3)
+    {
+      printf(" Usage : ./auto_exec.sh <numBlocks> <numData>\n");
+      exit(0);
+    }
+  if (argc > 1)
+    {
+      numBlocks = atoi(argv[1]);
+      numData = atoi(argv[2]);
+    }
 
 
+  /** Declare the Filter Properties */
+  numTap = 1024;
+  numTotalData = numData * numBlocks;
+  local = 64;
 
-	// Event Creation
-	cl_event event;
-	EventList* eventList;
+  printf("FIR Filter\n Data Samples : %d \n NumBlocks : %d \n Local Workgroups : %d\n", numData,numBlocks,local);
+  //exit(0);
+  /** Define variables here */
+  input = (cl_float *) malloc( numTotalData* sizeof(cl_float) );
+  output = (cl_float *) malloc( numTotalData* sizeof(cl_float) );
+  coeff = (cl_float *) malloc( numTap* sizeof(cl_float) );
+  temp_output = (cl_float *) malloc( (numData+numTap-1) * sizeof(cl_float) );
+
+  /** Initialize the input data */
+  for( i=0;i<numTotalData;i++ )
+    {
+      input[i] = 8;
+      output[i] = 99;
+    }
+
+  for( i=0;i<numTap;i++ )
+    coeff[i] = 1.0/numTap;
+
+  for( i=0; i<(numData+numTap-1); i++ )
+    temp_output[i] = 0.0;
+
+
+
+  // Event Creation
+  cl_event event;
+  EventList* eventList;
 
 #if 1
-	// Read the input file
-	FILE *fip;
-	i=0;
-	fip = fopen("temp.dat","r");
-	while(i<numTotalData)
-	{
-		int res = fscanf(fip,"%f",&input[i]);
-		i++;
-	}
-	fclose(fip);
+  // Read the input file
+  FILE *fip;
+  i=0;
+  fip = fopen("temp.dat","r");
+  while(i<numTotalData)
+    {
+      int res = fscanf(fip,"%f",&input[i]);
+      i++;
+    }
+  fclose(fip);
 
 #if 0
-	printf("\n The Input:\n");
-	i = 0;
-	while( i<numTotalData )
-	{
-		printf( "%f, ", input[i] );
+  printf("\n The Input:\n");
+  i = 0;
+  while( i<numTotalData )
+    {
+      printf( "%f, ", input[i] );
 
-		i++;
-	}
+      i++;
+    }
 #endif
 #endif
 
-	// Load the kernel source code into the array source_str
-	FILE *fp;
-	char *source_str;
-	size_t source_size;
+  // Load the kernel source code into the array source_str
+  FILE *fp;
+  char *source_str;
+  size_t source_size;
 
-	fp = fopen("FIR.cl", "r");
-	if (!fp) {
-		fprintf(stderr, "Failed to load kernel.\n");
-		exit(1);
-	}
-	source_str = (char*)malloc(MAX_SOURCE_SIZE);
-	source_size = fread( source_str, 1, MAX_SOURCE_SIZE, fp);
-	fclose( fp );
+  fp = fopen("FIR.cl", "r");
+  if (!fp) {
+    fprintf(stderr, "Failed to load kernel.\n");
+    exit(1);
+  }
+  source_str = (char*)malloc(MAX_SOURCE_SIZE);
+  source_size = fread( source_str, 1, MAX_SOURCE_SIZE, fp);
+  fclose( fp );
 
-	// Get platform and device information
-	cl_platform_id platform_id = NULL;
-	cl_device_id device_id = NULL;
-	cl_uint ret_num_devices;
-	cl_uint ret_num_platforms;
-	cl_int ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
-	ret = clGetDeviceIDs( platform_id, CL_DEVICE_TYPE_ALL, 1,
+  // Get platform and device information
+  cl_platform_id platform_id = NULL;
+  cl_device_id device_id = NULL;
+  cl_uint ret_num_devices;
+  cl_uint ret_num_platforms;
+  cl_int ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
+  ret = clGetDeviceIDs( platform_id, CL_DEVICE_TYPE_ALL, 1,
 			&device_id, &ret_num_devices);
 
-	printf("/n No of Devices %d",ret_num_platforms );
+  printf("/n No of Devices %d",ret_num_platforms );
 
-	char *platformVendor;
-	size_t platInfoSize;
-	clGetPlatformInfo(platform_id, CL_PLATFORM_VENDOR, 0, NULL,
-			&platInfoSize);
+  char *platformVendor;
+  size_t platInfoSize;
+  clGetPlatformInfo(platform_id, CL_PLATFORM_VENDOR, 0, NULL,
+		    &platInfoSize);
 
-	platformVendor = (char*)malloc(platInfoSize);
+  platformVendor = (char*)malloc(platInfoSize);
 
-	clGetPlatformInfo(platform_id, CL_PLATFORM_VENDOR, platInfoSize,
-			platformVendor, NULL);
-	printf("\tVendor: %s\n", platformVendor);
-	free(platformVendor);
+  clGetPlatformInfo(platform_id, CL_PLATFORM_VENDOR, platInfoSize,
+		    platformVendor, NULL);
+  printf("\tVendor: %s\n", platformVendor);
+  free(platformVendor);
 
-	// Create an OpenCL context
-	cl_context context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
+  // Create an OpenCL context
+  cl_context context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
 
-	// Create a command queue
-	cl_command_queue command_queue = clCreateCommandQueueWithProperties(context, device_id, NULL, &ret);
+  // Create a command queue
+  //cl_command_queue command_queue = clCreateCommandQueueWithProperties(context, device_id, NULL, &ret);
+  cl_queue_properties props[] = {CL_QUEUE_PROPERTIES, CL_QUEUE_PROFILING_ENABLE, 0};
+  cl_command_queue command_queue = clCreateCommandQueueWithProperties(context, device_id, props, &ret);
 
 	
-	// Create Eventlist for Timestamps
-	eventList = new EventList(context, command_queue, device_id,true);
+  // Create Eventlist for Timestamps
+  eventList = new EventList(context, command_queue, device_id,true);
 
-//#ifdef GPUPROF
-//	// Create performance counter Init
-//	GPA_Initialize();
-//	GPA_OpenContext( command_queue );
-//	GPA_EnableAllCounters();
-//#endif
+  //#ifdef GPUPROF
+  //	// Create performance counter Init
+  //	GPA_Initialize();
+  //	GPA_OpenContext( command_queue );
+  //	GPA_EnableAllCounters();
+  //#endif
 
-	// Create memory buffers on the device for each vector
-	cl_mem inputBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY,
-			sizeof(cl_float) * numData, NULL, &ret);
-	cl_mem outputBuffer = clCreateBuffer(context, CL_MEM_READ_WRITE,
-			sizeof(cl_float) * numData, NULL, &ret);
-	cl_mem coeffBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY,
-			sizeof(cl_float) * numTap, NULL, &ret);
-	cl_mem temp_outputBuffer = clCreateBuffer(context, CL_MEM_READ_WRITE,
-			sizeof(cl_float) * (numData+numTap-1), NULL, &ret);
+  // Create memory buffers on the device for each vector
+  cl_mem inputBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY,
+				      sizeof(cl_float) * numData, NULL, &ret);
+  cl_mem outputBuffer = clCreateBuffer(context, CL_MEM_READ_WRITE,
+				       sizeof(cl_float) * numData, NULL, &ret);
+  cl_mem coeffBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY,
+				      sizeof(cl_float) * numTap, NULL, &ret);
+  cl_mem temp_outputBuffer = clCreateBuffer(context, CL_MEM_READ_WRITE,
+					    sizeof(cl_float) * (numData+numTap-1), NULL, &ret);
 
-	// Create a program from the kernel source
-	cl_program program = clCreateProgramWithSource(context, 1,
-			(const char **)&source_str, (const size_t *)&source_size, &ret);
+  // Create a program from the kernel source
+  cl_program program = clCreateProgramWithSource(context, 1,
+						 (const char **)&source_str, (const size_t *)&source_size, &ret);
 
-	// Build the program
-	ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
+  // Build the program
+  ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
 
-	CHECK_STATUS( ret,"Error: Build Program\n");
+  CHECK_STATUS( ret,"Error: Build Program\n");
 
-	// Create the OpenCL kernel
-	cl_kernel kernel = clCreateKernel(program, "FIR", &ret);
-	CHECK_STATUS( ret,"Error: Create kernel. (clCreateKernel)\n");
-
-
-	// Set the arguments of the kernel
-	ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&outputBuffer);
-	ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&coeffBuffer);
-	ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&temp_outputBuffer);
-	ret = clSetKernelArg(kernel, 3, sizeof(cl_uint), (void *)&numTap);
-
-	// Initialize Memory Buffer
-	ret = clEnqueueWriteBuffer(command_queue,
-			coeffBuffer,
-			1,
-			0,
-			numTap * sizeof(cl_float),
-			coeff,
-			0,
-			0,
-			&event);
-
-	eventList->add(event);
-
-	ret = clEnqueueWriteBuffer(command_queue,
-			temp_outputBuffer,
-			1,
-			0,
-			(numTap) *sizeof(cl_float),
-			temp_output,
-			0,
-			0,
-			&event);
-
-	eventList->add(event);
+  // Create the OpenCL kernel
+  cl_kernel kernel = clCreateKernel(program, "FIR", &ret);
+  CHECK_STATUS( ret,"Error: Create kernel. (clCreateKernel)\n");
 
 
+  // Set the arguments of the kernel
+  ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&outputBuffer);
+  ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&coeffBuffer);
+  ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&temp_outputBuffer);
+  ret = clSetKernelArg(kernel, 3, sizeof(cl_uint), (void *)&numTap);
 
-	// Decide the local group formation
-	size_t globalThreads[1]={numData};
-	size_t localThreads[1]={128};
-	cl_command_type cmdType;
-	count = 0;
-	while( count < numBlocks )
-	{
+  // Initialize Memory Buffer
+  ret = clEnqueueWriteBuffer(command_queue,
+			     coeffBuffer,
+			     1,
+			     0,
+			     numTap * sizeof(cl_float),
+			     coeff,
+			     0,
+			     0,
+			     &event);
 
-		/* fill in the temp_input buffer object */
-		ret = clEnqueueWriteBuffer(command_queue,
-				temp_outputBuffer,
-				1,
-				(numTap-1)*sizeof(cl_float),
-				numData * sizeof(cl_float),
-				input + (count * numData),
-				0,
-				0,
-				&event);
+  eventList->add(event);
 
-		//(numTap-1)*sizeof(cl_float)
-		eventList->add(event);
+  ret = clEnqueueWriteBuffer(command_queue,
+			     temp_outputBuffer,
+			     1,
+			     0,
+			     (numTap) *sizeof(cl_float),
+			     temp_output,
+			     0,
+			     0,
+			     &event);
 
-
-		size_t global_item_size = numData; // GLOBAL ITEMSIZE IS CUSTOM BASED ON COMPUTAION ALGO
-		size_t local_item_size = numData;
-		//		size_t local_item_size[4] = {numData/4,numData/4,numData/4,numData/4}; // LOCAL ITEM SIZE IS CUSTOM BASED ON COMPUTATION ALGO
-
-//#if GPUPROF
-//
-//
-//		//Session Creation
-//		static gpa_uint32 currentWaitSessionID = 1;
-//		gpa_uint32 sessionID;
-//		GPA_BeginSession( &sessionID );
-//
-//		// Required number of passes can be custom
-//		gpa_uint32 numRequiredPasses = 1;
-//
-//
-//		GPA_GetPassCount( &numRequiredPasses );
-//		for ( gpa_uint32 i = 0; i < numRequiredPasses; i++ )
-//		{
-//			GPA_BeginPass();
-//			GPA_BeginSample( 0 );
-//
-//#endif
-//
-			// Execute the OpenCL kernel on the list
-			ret = clEnqueueNDRangeKernel(
-					command_queue,
-					kernel,
-					1,
-					NULL,
-					globalThreads,
-					localThreads,
-					0,
-					NULL,
-					&event);
-
-
-			CHECK_STATUS( ret,"Error: Range kernel. (clCreateKernel)\n");
-			ret = clWaitForEvents(1, &event);
-			ret = clWaitForEvents(1, &event);
+  eventList->add(event);
 
 
 
+  // Decide the local group formation
+  size_t globalThreads[1]={numData};
+  size_t localThreads[1]={128};
+  cl_command_type cmdType;
+  count = 0;
+  while( count < numBlocks )
+    {
 
-//#if GPUPROF
-//			// End Profile session
-//			GPA_EndSample();
-//			GPA_EndPass();
-//
-//			eventList->add(event);
-//		}
-//		GPA_EndSession();
-//
-//		bool readyResult = true;
-//		if ( sessionID != currentWaitSessionID )
-//		{
-//			GPA_Status sessionStatus;
-//			sessionStatus = GPA_IsSessionReady( &readyResult,
-//					currentWaitSessionID );
-//			while ( sessionStatus == GPA_STATUS_ERROR_SESSION_NOT_FOUND )
-//			{
-//				// skipping a session which got overwritten
-//				currentWaitSessionID++;
-//				sessionStatus = GPA_IsSessionReady( &readyResult,
-//						currentWaitSessionID );
-//			}
-//		}
-//		if ( readyResult )
-//		{
-//			WriteSession( currentWaitSessionID,
-//					"Results1.log" );
-//			currentWaitSessionID++;
-//		}
-//		//Cleanup
-//		GPA_CloseContext();
-//
-//#endif
+      /* fill in the temp_input buffer object */
+      ret = clEnqueueWriteBuffer(command_queue,
+				 temp_outputBuffer,
+				 1,
+				 (numTap-1)*sizeof(cl_float),
+				 numData * sizeof(cl_float),
+				 input + (count * numData),
+				 0,
+				 0,
+				 &event);
 
-		eventList->add(event);
+      //(numTap-1)*sizeof(cl_float)
+      eventList->add(event);
 
-		/* Get the output buffer */
-		ret = clEnqueueReadBuffer(
+
+      size_t global_item_size = numData; // GLOBAL ITEMSIZE IS CUSTOM BASED ON COMPUTAION ALGO
+      size_t local_item_size = numData;
+      //		size_t local_item_size[4] = {numData/4,numData/4,numData/4,numData/4}; // LOCAL ITEM SIZE IS CUSTOM BASED ON COMPUTATION ALGO
+
+      //#if GPUPROF
+      //
+      //
+      //		//Session Creation
+      //		static gpa_uint32 currentWaitSessionID = 1;
+      //		gpa_uint32 sessionID;
+      //		GPA_BeginSession( &sessionID );
+      //
+      //		// Required number of passes can be custom
+      //		gpa_uint32 numRequiredPasses = 1;
+      //
+      //
+      //		GPA_GetPassCount( &numRequiredPasses );
+      //		for ( gpa_uint32 i = 0; i < numRequiredPasses; i++ )
+      //		{
+      //			GPA_BeginPass();
+      //			GPA_BeginSample( 0 );
+      //
+      //#endif
+      //
+      // Execute the OpenCL kernel on the list
+      ret = clEnqueueNDRangeKernel(
+				   command_queue,
+				   kernel,
+				   1,
+				   NULL,
+				   globalThreads,
+				   localThreads,
+				   0,
+				   NULL,
+				   &event);
+
+
+      CHECK_STATUS( ret,"Error: Range kernel. (clCreateKernel)\n");
+      ret = clWaitForEvents(1, &event);
+      ret = clWaitForEvents(1, &event);
+
+
+
+
+      //#if GPUPROF
+      //			// End Profile session
+      //			GPA_EndSample();
+      //			GPA_EndPass();
+      //
+      //			eventList->add(event);
+      //		}
+      //		GPA_EndSession();
+      //
+      //		bool readyResult = true;
+      //		if ( sessionID != currentWaitSessionID )
+      //		{
+      //			GPA_Status sessionStatus;
+      //			sessionStatus = GPA_IsSessionReady( &readyResult,
+      //					currentWaitSessionID );
+      //			while ( sessionStatus == GPA_STATUS_ERROR_SESSION_NOT_FOUND )
+      //			{
+      //				// skipping a session which got overwritten
+      //				currentWaitSessionID++;
+      //				sessionStatus = GPA_IsSessionReady( &readyResult,
+      //						currentWaitSessionID );
+      //			}
+      //		}
+      //		if ( readyResult )
+      //		{
+      //			WriteSession( currentWaitSessionID,
+      //					"Results1.log" );
+      //			currentWaitSessionID++;
+      //		}
+      //		//Cleanup
+      //		GPA_CloseContext();
+      //
+      //#endif
+
+      eventList->add(event);
+
+      /* Get the output buffer */
+      ret = clEnqueueReadBuffer(
 				command_queue,
 				outputBuffer,
 				CL_TRUE,
@@ -339,50 +348,54 @@ int main(int argc , char** argv) {
 				0,
 				NULL,
 				&event );
-		eventList->add(event);
-		count ++;
-	}
+      eventList->add(event);
+      count ++;
+    }
 
-	/* Uncomment to trint output */
-	//printf("\n The Output:\n");
-	//i = 0;
-	//while( i<numTotalData )
-	//{
-	//	printf( "%f ", output[i] );
+  /* Uncomment to trint output */
+  //printf("\n The Output:\n");
+  //i = 0;
+  //while( i<numTotalData )
+  //{
+  //	printf( "%f ", output[i] );
 
-	//	i++;
-	//}
+  //	i++;
+  //}
 
-	ret = clFlush(command_queue);
-	ret = clFinish(command_queue);
-	ret = clReleaseKernel(kernel);
-	ret = clReleaseProgram(program);
-	ret = clReleaseMemObject(inputBuffer);
-	ret = clReleaseMemObject(outputBuffer);
-	ret = clReleaseMemObject(coeffBuffer);
-	ret = clReleaseMemObject(temp_outputBuffer);
-	ret = clReleaseCommandQueue(command_queue);
-	ret = clReleaseContext(context);
+  ret = clFlush(command_queue);
+  ret = clFinish(command_queue);
+  ret = clReleaseKernel(kernel);
+  ret = clReleaseProgram(program);
+  ret = clReleaseMemObject(inputBuffer);
+  ret = clReleaseMemObject(outputBuffer);
+  ret = clReleaseMemObject(coeffBuffer);
+  ret = clReleaseMemObject(temp_outputBuffer);
+  ret = clReleaseCommandQueue(command_queue);
+  ret = clReleaseContext(context);
 
-	free(input);
-	free(output);
-	free(coeff);
-	free(temp_output);
+  free(input);
+  free(output);
+  free(coeff);
+  free(temp_output);
 
-//#ifdef GPUPROF
-//	GPA_Destroy();
-//#endif
+  //#ifdef GPUPROF
+  //	GPA_Destroy();
+  //#endif
 
-	/* comment to hide timing events */
-	eventList->printEvents();
-	eventList->dumpEvents((char *)"eventDumps");
-	delete eventList;
+  /* comment to hide timing events */
+  eventList->printEvents();
+  eventList->dumpEvents((char *)"eventDumps");
+  delete eventList;
 
-	c_stop = clock();
-	float diff = (((float)c_stop - (float)c_start) / CLOCKS_PER_SEC) * 1000;
-	printf("\nDone! - Time taken: %f\n", diff);
+  //	c_stop = clock();
+  // float diff = (((float)c_stop - (float)c_start) / CLOCKS_PER_SEC) * 1000;
+  // printf("\nDone! - Time taken: %f\n", diff);
 
-	return 0;
+  clock_gettime(CLOCK_MONOTONIC, &end);/* mark the end time */
+  diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+  printf("elapsed time = %llu nanoseconds\n", (long long unsigned int) diff);
+
+  return 0;
 }
 
 //#ifdef GPUPROF
