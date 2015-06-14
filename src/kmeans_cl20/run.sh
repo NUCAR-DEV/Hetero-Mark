d1c22691f2242a68b54c3baf6db281a0b0ce5884
saonim <sayani.mookherjee@gmail.com>
2015-06-13 21:40:12 -0400

    Fixed run.sh naming

--- src/kmeans_cl20/run.sh
+++ src/kmeans_cl20/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cp bin/x86_64/Release/kmeans .
-./kmeans -o -i ../data/kmeans/inpuGen/300_34.txt
-rm kmeans
+cp bin/x86_64/Release/kmeans20 .
+./kmeans20 -o -i ../data/kmeans/inpuGen/300_34.txt
+rm kmeans20
