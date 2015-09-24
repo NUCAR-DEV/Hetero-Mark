2c3b5075ecb023962c794c2dea6363f2a4c7efd7
Yifan Sun <yifansun@coe.ece.neu.edu>
2015-09-24 16:10:13 -0400

    Remove unused folder

--- src/opencl12/kmeans_cl12/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cp bin/x86_64/Release/kmeans12 .
-./kmeans12 -o -i ../data/kmeans/inpuGen/100000_34.txt 
-rm kmeans12
--- src/opencl20/pagerank_cl20/runAll.sh
+++
@@ -1,5 +0,0 @@
-for f in ./input/csrMatrix/*
-do
-	echo "$f"
-	./bin/x86_64/Release/pagerank $f
-done
