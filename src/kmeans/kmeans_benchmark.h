2c2b3f71a8a366d7bb41e6f3f532bd2d15cdab47
Xiang Gong <xgong@ece.neu.edu>
2016-03-11 10:48:59 -0500

    * Adding kmeans benchmark

---
+++ src/kmeans/kmeans_benchmark.h
@@ -0,0 +1,84 @@
+/*
+ * Hetero-mark
+ *
+ * Copyright (c) 2015 Northeastern University
+ * All rights reserved.
+ *
+ * Developed by:
+ *   Northeastern University Computer Architecture Research (NUCAR) Group
+ *   Northeastern University
+ *   http://www.ece.neu.edu/groups/nucar/
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal with the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ *   Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimers.
+ *
+ *   Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimers in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ *   Neither the names of NUCAR, Northeastern University, nor the names of
+ *   its contributors may be used to endorse or promote products derived
+ *   from this Software without specific prior written permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS WITH THE SOFTWARE.
+ */
+
+#ifndef SRC_KMEANS_KMEANS_BENCHMARK_H_
+#define SRC_KMEANS_KMEANS_BENCHMARK_H_
+
+#include <string>
+#include "src/common/benchmark/benchmark.h"
+#include "src/common/time_measurement/time_measurement.h"
+
+class KmeansBenchmark : public Benchmark {
+ protected:
+  std::string filename_ = "";
+  double threshold_ = 0.001;
+  uint32_t max_num_clusters_ = 5;
+  uint32_t min_num_clusters_ = 5;
+  uint32_t num_loops_ = 1;
+  bool is_rmse_ = false;
+  bool is_output_ = false;
+
+  uint32_t num_points_ = 0;
+  uint32_t num_features_ = 0;
+  uint32_t best_nclusters_ = 0;
+
+  std::unique_ptr<float[]> host_features_;
+
+ public:
+  void Initialize() override;
+  void Run() override = 0;
+  void Verify() override;
+  void Summarize() override;
+  void Cleanup() override;
+
+  // Setters
+  void setFilename(std::string filename) { filename_ = filename; }
+  void setThreshold(double threshold) { threshold_ = threshold; }
+  void setMaxNumClusters(uint32_t max_num_clusters) {
+    max_num_clusters_ = max_num_clusters;
+  }
+  void setMinNumClusters(uint32_t min_num_clusters) {
+    min_num_clusters_ = min_num_clusters;
+  }
+  void setIsRMSE(bool is_rmse) { is_rmse_ = is_rmse; }
+  void setIsOutput(bool is_output) { is_output_ = is_output; }
+  void setNumLoops(uint32_t num_loops) { num_loops_ = num_loops; }
+};
+
+#endif  // SRC_KMEANS_KMEANS_BENCHMARK_H_
