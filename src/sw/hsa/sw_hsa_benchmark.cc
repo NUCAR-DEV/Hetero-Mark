34e8f60c909feed95e87d91d79cd79c23432fada
Xiang Gong <xgong@ece.neu.edu>
2016-04-11 16:23:43 -0400

    * ShallowWater HSA

--- src/sw/hsa/sw_hsa_benchmark.cc
+++ src/sw/hsa/sw_hsa_benchmark.cc
@@ -44,6 +44,175 @@
 
 void SwHsaBenchmark::Initialize() {
   SwBenchmark::Initialize();
+
+  InitializeParams();
+  InitializeBuffers();
+  InitializeData();
+}
+
+void SwHsaBenchmark::InitBuffer() {
+  size_t sizeInBytes = sizeof(double) * m_len_ * n_len_;
+
+  // Fine grain buffers
+  u_curr_ = reinterpret_cast<double *>(malloc(sizeInBytes));
+  u_next_ = reinterpret_cast<double *>(malloc(sizeInBytes));
+  v_curr_ = reinterpret_cast<double *>(malloc(sizeInBytes));
+  v_next_ = reinterpret_cast<double *>(malloc(sizeInBytes));
+  p_curr_ = reinterpret_cast<double *>(malloc(sizeInBytes));
+  p_next_ = reinterpret_cast<double *>(malloc(sizeInBytes));
+  u_ = reinterpret_cast<double *>(malloc(sizeInBytes));
+  v_ = reinterpret_cast<double *>(malloc(sizeInBytes));
+  p_ = reinterpret_cast<double *>(malloc(sizeInBytes));
+  cu_ = reinterpret_cast<double *>(malloc(sizeInBytes));
+  cv_ = reinterpret_cast<double *>(malloc(sizeInBytes));
+  z_ = reinterpret_cast<double *>(malloc(sizeInBytes));
+  h_ = reinterpret_cast<double *>(malloc(sizeInBytes));
+  psi_ = reinterpret_cast<double *>(malloc(sizeInBytes));
+}
+
+void SwHsaBenchmark::FreeBuffer() {
+  free(u_curr_);
+  free(u_next_);
+  free(v_curr_);
+  free(v_next_);
+  free(p_curr_);
+  free(p_next_);
+  free(u_);
+  free(v_);
+  free(p_);
+  free(cu_);
+  free(cv_);
+  free(z_);
+  free(h_);
+  free(psi_);
+}
+
+void SwHsaBenchmark::InitPsiP() {
+  SNK_INIT_LPARM(lparm, 0);
+  lparm->gdims[0] = m_len_;
+  lparm->gdims[1] = m_len_;
+  lparm->ldims[0] = 16;
+  lparm->ldims[1] = 16;
+
+  sw_init_psi_p(a_, di_, dj_, pcf_, m_len_, m_len_, p_, psi_, lparm);
+}
+
+void SwHsaBenchmark::InitVelocities() {
+  SNK_INIT_LPARM(lparm, 0);
+  lparm->gdims[0] = m_;
+  lparm->gdims[1] = m_;
+  lparm->ldims[0] = 16;
+  lparm->ldims[1] = 16;
+
+  sw_init_velocities(dx_, dy_, m_, n_, psi_, u_, v_, lparm);
+}
+
+void SwHsaBenchmark::Compute0() {
+  SNK_INIT_LPARM(lparm, 0);
+  lparm->gdims[0] = m_len_;
+  lparm->gdims[1] = m_len_;
+  lparm->ldims[0] = 16;
+  lparm->ldims[1] = 16;
+
+  sw_compute0(fsdx_, fsdy_, m_len_, u_, v_, p_, cu_, cv_, z_, h_, lparm);
+}
+
+void SwHsaBenchmark::PeriodicUpdate0() {
+  SNK_INIT_LPARM(lparm, 0);
+  lparm->gdims[0] = m_;
+  lparm->gdims[1] = n_;
+  lparm->ldims[0] = 16;
+  lparm->ldims[1] = 16;
+
+  sw_update0(m_, n_, m_len_, cu_, cv_, z_, h_, lparm);
+}
+
+void SwHsaBenchmark::Compute1() {
+  tdts8_ = tdt_ / 8.;
+  tdtsdx_ = tdt_ / dx_;
+  tdtsdy_ = tdt_ / dy_;
+
+  SNK_INIT_LPARM(lparm, 0);
+  lparm->gdims[0] = m_len_;
+  lparm->gdims[1] = m_len_;
+  lparm->ldims[0] = 16;
+  lparm->ldims[1] = 16;
+
+  sw_compute1(tdts8_, tdtsdx_, tdtsdy_, m_len_, cu_, cv_, z_, h_, u_curr_,
+              v_curr_, p_curr_, u_next_, v_next_, p_next_, lparm);
+}
+
+void SwHsaBenchmark::PeriodicUpdate1() {
+  SNK_INIT_LPARM(lparm, 0);
+  lparm->gdims[0] = m_len_;
+  lparm->gdims[1] = m_len_;
+  lparm->ldims[0] = 16;
+  lparm->ldims[1] = 16;
+
+  sw_update1(m_, n_, m_len_, u_next_, v_next_, p_next_, lparm);
+}
+
+void SwHsaBenchmark::TimeSmooth(int ncycle) {
+  if (ncycle > 1) {
+    SNK_INIT_LPARM(lparm, 0);
+    lparm->gdims[0] = m_len_;
+    lparm->gdims[1] = m_len_;
+    lparm->ldims[0] = 16;
+    lparm->ldims[1] = 16;
+
+    sw_time_smooth(m_, n_, m_len_, alpha_, u_, v_, p_, u_curr_, v_curr_,
+                   p_curr_, u_next_, v_next_, p_next_, lparm);
+  } else {
+    tdt_ += tdt_;
+    size_t sizeInBytes = sizeof(double) * m_len_ * n_len_;
+
+    memcpy(u_curr_, u_, sizeInBytes);
+    memcpy(v_curr_, v_, sizeInBytes);
+    memcpy(p_curr_, p_, sizeInBytes);
+    memcpy(u_, u_next_, sizeInBytes);
+    memcpy(v_, v_next_, sizeInBytes);
+    memcpy(p_, p_next_, sizeInBytes);
+  }
+}
+
+void SwHsaBenchmark::InitializeParams() {
+  m_len_ = m_ + 1;
+  n_len_ = n_ + 1;
+  itmax_ = 250;
+  dt_ = 90.;
+  tdt_ = dt_;
+  dx_ = 100000.;
+  dy_ = 100000.;
+  a_ = 1000000.;
+  alpha_ = .001;
+  el_ = n_ * dx_;
+  pi_ = 4. * atanf(1.);
+  tpi_ = pi_ + pi_;
+  di_ = tpi_ / m_;
+  dj_ = tpi_ / n_;
+  pcf_ = pi_ * pi_ * a_ * a_ / (el_ * el_);
+  fsdx_ = 4. / dx_;
+  fsdy_ = 4. / dy_;
+}
+
+void SwHsaBenchmark::InitializeData() {
+  InitPsiP();
+  InitVelocities();
+
+  size_t sizeInBytes = sizeof(double) * m_len_ * n_len_;
+  memcpy(u_curr_, u_, sizeInBytes);
+  memcpy(v_curr_, v_, sizeInBytes);
+  memcpy(p_curr_, p_, sizeInBytes);
+}
+
+void SwHsaBenchmark::Run() {
+  for (unsigned i = 0; i < itmax_; ++i) {
+    Compute0();
+    PeriodicUpdate0();
+    Compute1();
+    PeriodicUpdate1();
+    TimeSmooth(i);
+  }
 }
 
 void SwHsaBenchmark::Run() {}
--- src/sw/hsa/sw_hsa_benchmark.h
+++ src/sw/hsa/sw_hsa_benchmark.h
@@ -45,6 +45,53 @@
 
 class SwHsaBenchmark : public SwBenchmark {
  private:
+  // HSA style buffers
+  double *u_curr_;
+  double *u_next_;
+
+  double *v_curr_;
+  double *v_next_;
+
+  double *p_curr_;
+  double *p_next_;
+
+  double *u_;
+  double *v_;
+  double *p_;
+
+  double *cu_;
+  double *cv_;
+
+  double *z_;
+  double *h_;
+  double *psi_;
+
+  // Kernels
+  cl_kernel kernel_sw_init_psi_p_;
+  cl_kernel kernel_sw_init_velocities_;
+  cl_kernel kernel_sw_compute0_;
+  cl_kernel kernel_sw_update0_;
+  cl_kernel kernel_sw_compute1_;
+  cl_kernel kernel_sw_update1_;
+  cl_kernel kernel_sw_time_smooth_;
+
+  // Initialize
+  void InitializeParams();
+  void InitializeData();
+  void InitializeBuffers();
+  void InitPsiP();
+  void InitVelocities();
+
+  // Free
+  void FreeBuffers();
+
+  // Run
+  void Compute0();
+  void PeriodicUpdate0();
+  void Compute1();
+  void PeriodicUpdate1();
+  void TimeSmooth(int ncycle);
+
  public:
   void Initialize() override;
   void Run() override;
