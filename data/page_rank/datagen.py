d05a5dcc3cfc7d986e0550d552dc1f3d19f47436
Yifan Sun <sunyifan112358@gmail.com>
2016-04-23 22:19:56 -0400

    Solve page rank bug by regenerate data file

--- data/page_rank/datagen.py
+++ data/page_rank/datagen.py
@@ -2,17 +2,17 @@ from scipy import sparse
 import numpy as np
 from scipy.sparse import csr_matrix
 
-def genCsrFile(numRows, numCols, density):
-    m = sparse.rand(numRows, numCols, density=density, format='csr', dtype=np.float32, random_state=None)
+def genCsrFile(numRows, numCols, density, filename):
+    m = sparse.rand(numRows, numCols, 
+            density=density, format='csr', 
+            dtype=np.float32, random_state=None)
     a = m.toarray()
     zeroCol = np.where(~a.any(axis=0))[0]
     a[0,zeroCol] = 1
     stoMatrix = np.divide(a, np.sum(a, axis=0))
     stoMatrix = csr_matrix(stoMatrix)
 
-#    fileName = './csrMatrix/' + 'csr_'+str(numRows)+'_'+str(int(density*100))+'.txt'
-    fileName = 'csr_'+str(numRows)+'_'+str(int(density*100))+'.txt'
-    f = open(fileName, 'w')
+    f = open(filename, 'w')
     print >> f, "{:d}".format(stoMatrix.nnz), "{:d}".format(stoMatrix.shape[0])
     for number in stoMatrix.indptr.tolist():
         print >> f, "{:d}".format(number),
@@ -21,9 +21,13 @@ def genCsrFile(numRows, numCols, density):
         print >> f, "{:d}".format(number),
     print >> f, '\n',
     for number in stoMatrix.data.tolist():
-        print >> f, "{:.2f}".format(number),
+        print >> f, "{:.20f}".format(number),
     print >> f, '\n',
     f.close()
-for numRows in range(1024,10240,1024):
-    for density in [0.1]:
-        genCsrFile(numRows, numRows, density)
+
+
+genCsrFile(64, 64, 0.05, 'tiny.data')
+genCsrFile(1024, 1024, 0.1, 'small.data')
+genCsrFile(4096, 4096, 0.1, 'medium.data')
+genCsrFile(16384, 16384, 0.1, 'large.data')
+
--- src/pr/pr_benchmark.cc
+++ src/pr/pr_benchmark.cc
@@ -109,7 +109,7 @@ void PrBenchmark::Verify() {
   bool has_error = false;
   for (i = 0; i < num_nodes_; i++) {
     if (isnan(page_rank_[i]) ||
-        fabs(page_rank_[i] - cpu_page_rank[i]) > page_rank_[i] / 100) {
+        fabs(page_rank_[i] - cpu_page_rank[i]) > cpu_page_rank[i] / 1000) {
       printf("Error with node %i, expected to be %e, but was %e\n", i,
              cpu_page_rank[i], page_rank_[i]);
       has_error = true;
