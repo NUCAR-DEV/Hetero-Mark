5f9dbdecae69add79aaf056f957e551cc5a2e2a3
Yifan Sun <sunyifan112358@gmail.com>
2017-02-13 21:48:07 -0500

    AES works again but not giving right result

--- cmake/HSA.cmake
+++ cmake/HSA.cmake
@@ -1,18 +1,15 @@
 # Check if snack is available
-find_file(have_snack NAMES snackhsail.sh DOC "snackhsail.sh file") 
+find_file(have_snack NAMES snack.sh DOC "snack.sh file") 
 
 # Check if HSA runtime is available
-set(HSA_RUNTIME_PATH "/opt/rocm/hsa")
+set(HSA_RUNTIME_PATH "/opt/rocm/")
 find_library(HSA_RUNTIME 
   NAMES hsa-runtime64
   PATHS ${HSA_RUNTIME_PATH}/lib
   NO_DEFAULT_PATH)
 
 if (have_snack AND HSA_RUNTIME)
-  set(SNACK "snackhsail.sh")
+  set(SNACK "snack.sh")
 else (have_snack AND HSA_RUNTIME)
-  message(${HSA_RUNTIME_PATH})
-  message(${have_snack})
-  message(${HSA_RUNTIME})
   message("snackhsail.sh or hsa runtime not found, skipping HSA benchmarks")
 endif ()
