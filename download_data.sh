992790064672d8828004e67c07bac40bdce721af
Yifan Sun <sunyifan112358@gmail.com>
2017-07-10 21:58:01 -0400

    New standard data files

--- download_data.sh
+++ download_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget http://nucarsrv.ece.neu.edu:40080/s/V56jG7q34E4vBIq/download -O data.tar.gz
+wget http://nucarsrv.ece.neu.edu:40080/s/hpnAC0VpqqocJ7F/download -O data.tar.gz
 tar -xzf data.tar.gz 
-
+rm data.tar.gz
