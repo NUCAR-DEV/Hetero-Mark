fcd3603b37002f7d05ce160d9f223cc946a409c9
Yifan Sun <sunyifan112358@gmail.com>
2017-07-24 14:24:44 -0400

    recover style check

--- cmake/StyleCheck.cmake
+++ cmake/StyleCheck.cmake
@@ -17,11 +17,23 @@ if(NOT CLANG_FORMAT)
 	message ("clang-format not found")
 endif()
 
+find_file(CPP_LINT_PY 
+  NAMES cpplint.py 
+  HINTS ${CMAKE_SOURCE_DIR}
+  DOC "Google cpp style scan program.")
+if(NOT CPP_LINT_PY)
+  message ("cpplint.py not found")
+endif()
+
 add_custom_target(check
 	COMMAND "${CMAKE_COMMAND}" -E chdir
-	"${CMAKE_SOURCE_DIR}"
-	clang-format -style=file -i
-	${ALL_SOURCE_FILES}
+		"${CMAKE_SOURCE_DIR}"
+		clang-format -style=file -i
+		${ALL_SOURCE_FILES}
+	COMMAND "${CMAKE_COMMAND}" 
+		-E chdir "${CMAKE_SOURCE_DIR}"
+		${CMAKE_SOURCE_DIR}/cpplint.py
+		${ALL_SOURCE_FILES}
 	DEPENDS ${AlL_SOURCE_FILES}
 	COMMENT "Linting source code"
 	VERBATIM)
