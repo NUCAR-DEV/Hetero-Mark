2c103cf6015494c4cab9176bd393ff9a3af13405
saonim <sayani.mookherjee@gmail.com>
2018-03-19 04:01:40 +0000

    Remove // be_cl12_benchmark.cc

--- src/be/cl12/be_cl12_benchmark.cc
+++ src/be/cl12/be_cl12_benchmark.cc
@@ -327,7 +327,6 @@ void BeCl12Benchmark::CollaborativeRun() {
     }
     frame_count++;
 
-    // printf("Frame started %d\n", frame_count);
     std::lock_guard<std::mutex> lk(queue_mutex_);
     frame_queue_.push(frame);
     queue_condition_variable_.notify_all();
@@ -335,13 +334,11 @@ void BeCl12Benchmark::CollaborativeRun() {
 
   {
     std::lock_guard<std::mutex> lk(queue_mutex_);
-    // printf("Finished.\n");
     finished_ = true;
   }
   queue_condition_variable_.notify_all();
 
   gpuThread.join();
-  // cudaStreamSynchronize(stream_);
 }
 
 void BeCl12Benchmark::GPUThread() {
